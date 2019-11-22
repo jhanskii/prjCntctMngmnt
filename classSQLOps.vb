c8ee30d369254944945c3c7dc23b4c60dca27b17
Ferdinand Paguio <paguioferdinand@gmail.com>
2019-11-22 17:28:26 +0800

    update

--- classSQLOps.vb
+++ classSQLOps.vb
@@ -1,3 +1,15 @@
 ﻿Public Class classSQLOps
+    Dim dbase As New classSQLOps
+
+    'Try
+    Dim dataset As New DataSet
+    Dim queryresult As String = ""
+    'dataset = mydb.executeSQL_dset("SELECT COMMAND", queryresult)
+    'GridView1.DataSource = dataset
+    'GridView1.DataBind()
+    'Catch ex As Exception
+
+    'End Try
+
 
 End Class
