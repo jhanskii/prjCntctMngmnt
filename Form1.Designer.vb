cdcb28170ef0d60471c9cc4720f279aee82cfac5
Jhanskii <m.nobora.recruitment@gmail.com>
2019-11-22 16:10:28 +0800

    login

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,92 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lbl_LoginHeading = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl_LoginHeading
+        '
+        Me.lbl_LoginHeading.AutoSize = True
+        Me.lbl_LoginHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_LoginHeading.Location = New System.Drawing.Point(12, 9)
+        Me.lbl_LoginHeading.Name = "lbl_LoginHeading"
+        Me.lbl_LoginHeading.Size = New System.Drawing.Size(80, 31)
+        Me.lbl_LoginHeading.TabIndex = 0
+        Me.lbl_LoginHeading.Text = "Login"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(112, 109)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(188, 41)
+        Me.btnLogin.TabIndex = 1
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(112, 49)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(188, 20)
+        Me.txtUsername.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(112, 83)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(188, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(48, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Username:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Password:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(347, 170)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.lbl_LoginHeading)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "frmLogin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lbl_LoginHeading As Label
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
 
+    End Sub
 End Class
