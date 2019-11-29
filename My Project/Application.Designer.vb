908d25e7ce9117c6fc488aed8a9756b836997ae8
Ferdinand Paguio <paguioferdinand@gmail.com>
2019-11-29 11:03:39 +0800

    login

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.prjCntctMngmnt.frmMain
+            Me.MainForm = Global.prjCntctMngmnt.frmLogin
         End Sub
     End Class
 End Namespace
